21fa268bf46089fcfd131cdd53c1d0360b5de140
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2024-11-05 09:55:35 -0500

    Initial commit

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -10,24 +10,27 @@ import homework.word_count as wc
 def test_01():
     """Test Word Count"""
 
-    wc.run_job("input", "output")
+    wc.run_job(
+        "files/input",
+        "files/output",
+    )
 
     #
     # Retorna error si la carpeta output/ no existe
-    if not os.path.exists("output/"):
+    if not os.path.exists("files/output/"):
         raise Exception("Output directory does not exist")
 
     #
     # Retorna error si el archivo "_SUCCESS" no existe en la
     # carpeta output/
-    if not os.path.exists("output/_SUCCESS"):
+    if not os.path.exists("files/output/_SUCCESS"):
         raise Exception("Output directory is empty")
 
     #
     # Lee el contenido del archivo "part-00000" en la carpeta output/
     # Cada linea en el archivo esta conformada por una clave un valor,
     # separados por un tabulador. Asigne pareja a un diccionario
-    with open("output/part-00000", "r", encoding="utf-8") as f:
+    with open("files/output/part-00000", "r", encoding="utf-8") as f:
         lines = f.readlines()
         result = {}
         for line in lines:
