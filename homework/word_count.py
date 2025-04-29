a2ed4c8d1b3c676dee2fe5553302bf8f1500bf42
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2025-04-28 22:47:14 -0500

    Update word_count.py

--- homework/word_count.py
+++ homework/word_count.py
@@ -8,121 +8,63 @@ import os.path
 import time
 from itertools import groupby
 
+from toolz.itertoolz import concat, pluck
 
-#
-# Escriba la funcion que  genere n copias de los archivos de texto en la
-# carpeta files/raw en la carpeta files/input. El nombre de los archivos
-# generados debe ser el mismo que el de los archivos originales, pero con
-# un sufijo que indique el número de copia. Por ejemplo, si el archivo
-# original se llama text0.txt, el archivo generado se llamará text0_1.txt,
-# text0_2.txt, etc.
-#
+
+
 def copy_raw_files_to_input_folder(n):
-    """Funcion copy_files"""
+    """Generate n copies of the raw files in the input folder"""
 
 
-#
-# Escriba la función load_input que recive como parámetro un folder y retorna
-# una lista de tuplas donde el primer elemento de cada tupla es el nombre del
-# archivo y el segundo es una línea del archivo. La función convierte a tuplas
-# todas las lineas de cada uno de los archivos. La función es genérica y debe
-# leer todos los archivos de folder entregado como parámetro.
-#
-# Por ejemplo:
-#   [
-#     ('text0'.txt', 'Analytics is the discovery, inter ...'),
-#     ('text0'.txt', 'in data. Especially valuable in ar...').
-#     ...
-#     ('text2.txt'. 'hypotheses.')
-#   ]
-#
+
 def load_input(input_directory):
     """Funcion load_input"""
 
 
-#
-# Escriba la función line_preprocessing que recibe una lista de tuplas de la
-# función anterior y retorna una lista de tuplas (clave, valor). Esta función
-# realiza el preprocesamiento de las líneas de texto,
-#
-def line_preprocessing(sequence):
-    """Line Preprocessing"""
+def preprocess_line(x):
+    """Preprocess the line x"""
 
 
-#
-# Escriba una función llamada maper que recibe una lista de tuplas de la
-# función anterior y retorna una lista de tuplas (clave, valor). En este caso,
-# la clave es cada palabra y el valor es 1, puesto que se está realizando un
-# conteo.
-#
-#   [
-#     ('Analytics', 1),
-#     ('is', 1),
-#     ...
-#   ]
-#
+def map_line(x):
+    pass
+
 def mapper(sequence):
     """Mapper"""
 
 
-#
-# Escriba la función shuffle_and_sort que recibe la lista de tuplas entregada
-# por el mapper, y retorna una lista con el mismo contenido ordenado por la
-# clave.
-#
-#   [
-#     ('Analytics', 1),
-#     ('Analytics', 1),
-#     ...
-#   ]
-#
 def shuffle_and_sort(sequence):
     """Shuffle and Sort"""
 
 
-#
-# Escriba la función reducer, la cual recibe el resultado de shuffle_and_sort y
-# reduce los valores asociados a cada clave sumandolos. Como resultado, por
-# ejemplo, la reducción indica cuantas veces aparece la palabra analytics en el
-# texto.
-#
+
+def compute_sum_by_group(group):
+    pass
+
 def reducer(sequence):
     """Reducer"""
 
 
-#
-# Escriba la función create_ouptput_directory que recibe un nombre de
-# directorio y lo crea. Si el directorio existe, lo borra
-#
-def create_ouptput_directory(output_directory):
+def create_directory(directory):
     """Create Output Directory"""
 
 
-#
-# Escriba la función save_output, la cual almacena en un archivo de texto
-# llamado part-00000 el resultado del reducer. El archivo debe ser guardado en
-# el directorio entregado como parámetro, y que se creo en el paso anterior.
-# Adicionalmente, el archivo debe contener una tupla por línea, donde el primer
-# elemento es la clave y el segundo el valor. Los elementos de la tupla están
-# separados por un tabulador.
-#
 def save_output(output_directory, sequence):
     """Save Output"""
 
 
-#
-# La siguiente función crea un archivo llamado _SUCCESS en el directorio
-# entregado como parámetro.
-#
 def create_marker(output_directory):
     """Create Marker"""
 
 
-#
-# Escriba la función job, la cual orquesta las funciones anteriores.
-#
 def run_job(input_directory, output_directory):
     """Job"""
+    sequence = load_input(input_directory)
+    sequence = mapper(sequence)
+    sequence = shuffle_and_sort(sequence)
+    sequence = reducer(sequence)
+    create_directory(output_directory)
+    save_output(output_directory, sequence)
+    create_marker(output_directory)
 
 
 if __name__ == "__main__":
