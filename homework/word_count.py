fe60716606f0fc72b9e71920a47450d324ab0360
tacevedooo <tacevedo@unal.edu.co>
2025-09-23 11:49:24 -0500

    solved

--- homework/word_count.py
+++ homework/word_count.py
@@ -14,6 +14,7 @@ from toolz.itertoolz import concat, pluck
 
 
 
+
 def copy_raw_files_to_input_folder(n):
     """Generate n copies of the raw files in the input folder"""
     raw_folder = "files/raw"
